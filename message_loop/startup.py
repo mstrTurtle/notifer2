3f117c5c5c0897f8452574585c7dd7600c4b5046
turtle <769711153@qq.com>
2024-04-12 13:43:04 +0800

    基于通用SIGINT和asyncio.Future、when_any的优雅退出

--- message_loop/startup.py
+++ message_loop/startup.py
@@ -15,11 +15,12 @@ from threading import Thread
 from vendor.mail.mailqq import senderSend
 import data.models as mo
 
-q = asyncio.Queue()
+q:asyncio.Queue = None
+fCancel: asyncio.Future = None
 cancel = None
 loop = asyncio.new_event_loop()
 
-def recover():
+def _recover():
     '''Recover from unexpected shutdown'''
     import data.models as mo
     db = mo.Message.db
@@ -30,18 +31,24 @@ def recover():
             val.status='idle'
             db[k] = val
 
-def on_sigint(signum, frame):
-    fCancel.set_result(object())
+sig_flag = False
+
+def _on_sigint(signum, frame):
+    print('on sigint')
+    global sig_flag
+    if not sig_flag:
+        fCancel.set_result(object())
+        sig_flag = True
 
 
-def onSendTimeout(m, ret):
+def _onSendTimeout(m, ret):
     print(f'timeout during sending "{m}"')
     e = mo.newEvent(message=m, detail = f'timeout during sending "{m}", ret = {ret}')
     mo.saveEvent(e)
     m.status = 'fail'
     mo.saveMessage(m)
 
-def onSendSuccess(m,ret):
+def _onSendSuccess(m,ret):
     e = mo.newEvent(message=m, detail = f'successfully sent "{m}", ret = {ret}')
     mo.saveEvent(e)
     print(f'* successfully sent "{m}", ret = {ret}')
@@ -51,26 +58,42 @@ def onSendSuccess(m,ret):
         m.status = 'fail'
     mo.saveMessage(m)
 
-async def sendMessage(m, p):
+async def _sendMessage(m, p):
     '''发送的任务'''
     print(f'* trying to send "{m}"')
     t = loop.run_in_executor(p, senderSend, m.email_title, m.email_content)
     print(f'* running senderSend thread on "{m}"')
     try:
         ret = await asyncio.wait_for(t, timeout=5)
-        onSendSuccess(m,ret)
+        _onSendSuccess(m,ret)
     except asyncio.TimeoutError:
-        onSendTimeout(m,ret)
+        _onSendTimeout(m,ret)
     
 def run_loop():
     
     print('in main')
     import signal
-    loop.add_signal_handler(signal.SIGINT, on_sigint)
-    loop.add_signal_handler(signal.SIGTERM, on_sigint)
+    # loop.add_signal_handler(signal.SIGINT, on_sigint)
+    # loop.add_signal_handler(signal.SIGTERM, on_sigint)
 
-    recover()
-    loop.run_until_complete(mainCoro())
+    
+    signal.signal(signal.SIGINT, _on_sigint)
+    signal.signal(signal.SIGTERM, _on_sigint)
+
+    _recover()
+
+    # while True:
+    try:
+        # 如果你用这个run_until_complete，你得额外包裹一层cancel, supress, run之类的东西才能正确处理KeyInterrupt之类的。
+        # https://stackoverflow.com/a/58532304/2073595 Python3.7+之后建议直接使用asyncio.run().
+        # loop.run_until_complete(mainCoro()) 
+        asyncio.set_event_loop(loop)
+        asyncio.run(_mainCoro()) # 这是run_until_complete加强易用版本。在其外面包了一层supress，再次运行等的东西
+    except KeyboardInterrupt:
+        print("on key intr")
+        _on_sigint(None,None)
+        # import os
+        # os._exit(42)
 
 def run_loop_with_new_thread()->Thread:
     thread = Thread(target = run_loop)
@@ -85,25 +108,32 @@ async def wait_until(dt):
         return
     await asyncio.sleep((dt - now).total_seconds())
 
-fCancel = asyncio.Future()
 
-async def waitAndDispatchOneMessage(m:mo.Message):
+async def _waitAndDispatchOneMessage(m:mo.Message):
     '''代表一个消息发送的长时任务，并且支持cancel'''
-    async def task():
+    async def dispatchCoro():
         try:
             await wait_until(m.send_time)
-            sendMessage(m)
+            _sendMessage(m)
         except asyncio.CancelledError:
             print(f'Message {m.id}\'s sending is gracefully closed')
+        task = dispatchCoro()
         await asyncio.wait([fCancel,task],return_when=asyncio.FIRST_COMPLETED)
    
 
-async def mainCoro():
+async def _mainCoro():
     '''负责不断取新消息的长时任务'''
+    global q, fCancel
+    q = asyncio.Queue()
+    fCancel = asyncio.Future()
     while True:
+        task = asyncio.create_task(q.get())
+        print('before wait')
+        finished, unfinished = await asyncio.wait([fCancel,task],return_when=asyncio.FIRST_COMPLETED) # recv a new Message
+        print('after wait')
         if fCancel.done():
             return
-        m: mo.Message = await q.get() # recv a new Message
-        mo.Message.db[m.id] = m # save it  # TODO: 同步的存储操作改成异步，并且用协程包装一下。
+        m: mo.Message = finished[0].result()
+        mo.Message.db[str(m.id)] = m # save it  # TODO: 同步的存储操作改成异步，并且用协程包装一下。
         cur_loop = asyncio.get_event_loop() # 拿到loop并且塞一个发送任务上去。
-        asyncio.run_coroutine_threadsafe(waitAndDispatchOneMessage(m), cur_loop)
+        asyncio.run_coroutine_threadsafe(_waitAndDispatchOneMessage(m), cur_loop)
