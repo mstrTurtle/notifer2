b52447cd7f0706ce34cfc25c64d0de941b71d038
turtle <769711153@qq.com>
2024-04-12 13:43:37 +0800

    让主线程跑asyncio而非uvicorn

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from message_loop.startup import loop, run_loop
 import uvicorn
 import threading
 
-t = threading.Thread(target=run_loop)
-uvicorn.run(app=app,port=8088)
+def run_server():
+    uvicorn.run(app=app,port=8088)
+
+t = threading.Thread(target=run_server)
 t.start()
+run_loop()
+print("run end")
 t.join()
+
+# run_loop()
